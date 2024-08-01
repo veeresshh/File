dae66593cc588e9d2c6113c19879b6f3e7315cb7
veeresshh <veeresh0270@gmail.com>
2024-08-01 14:15:05 +0530

    veeresh

--- src/FileUploadConvert.java
+++ src/FileUploadConvert.java
@@ -13,54 +13,51 @@ import org.testng.Assert;
 
 public class FileUploadConvert {
 
-
-	
 	@SuppressWarnings("deprecation")
-	
+
 	public static void main(String[] args) throws InterruptedException, IOException {
-			
-		String downloadPath=System.getProperty("user.dir");
-		
-		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Chromedriver\\chromedriver.exe");		
+
+		String downloadPath = System.getProperty("user.dir");
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Chromedriver\\chromedriver.exe");
 
 		HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
 		chromePrefs.put("profile.default_content_settings.popups", 0);
 		chromePrefs.put("download.default_directory", downloadPath);
-		ChromeOptions options=new ChromeOptions();
+		ChromeOptions options = new ChromeOptions();
 		options.setExperimentalOption("prefs", chromePrefs);
 
-		WebDriver Driver=new ChromeDriver(options);
+		WebDriver Driver = new ChromeDriver(options);
 		Driver.manage().window().maximize();
 		Driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-
 
 		Driver.get("https://www.ilovepdf.com/pdf_to_word");
 		Thread.sleep(3000);
 		Driver.findElement(By.id("pickfiles")).click();
 		Thread.sleep(3000);
 		Runtime.getRuntime().exec("C:\\Users\\VEERESH\\Documents\\FileUpload.exe");
-		
-		WebDriverWait wait = new WebDriverWait(Driver,Duration.ofSeconds(10));
+
+		WebDriverWait wait = new WebDriverWait(Driver, Duration.ofSeconds(10));
 
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("processTaskTextBtn")));
 		Driver.findElement(By.id("processTaskTextBtn")).click();
-		
+
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("pickfiles")));
 		Driver.findElement(By.id("pickfiles")).click();
-		
+
 		Thread.sleep(5000);
 
-		File f=new File(downloadPath+"//converted.zip");
+		File f = new File(downloadPath + "//converted.zip");
 
-		if(f.exists())
+		if (f.exists())
 
 		{
 
-		Assert.assertTrue(f.exists());
+			Assert.assertTrue(f.exists());
 
-		if(f.delete())
+			if (f.delete())
 
-		System.out.println("file deleted");
+				System.out.println("file deleted");
 
 		}
 	}
--- src/WindowPopUp.java
+++ src/WindowPopUp.java
@@ -7,13 +7,11 @@ public class WindowPopUp {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		
 		WebDriver Driver = new ChromeDriver();
 		Driver.manage().window().maximize();
 		Driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
-
-		Driver.get("https://admin:admin@the-internet.herokuapp.com");	
+		Driver.get("https://admin:admin@the-internet.herokuapp.com");
 		Driver.findElement(By.linkText("Basic Auth")).click();
 		Thread.sleep(3000);
 	}
